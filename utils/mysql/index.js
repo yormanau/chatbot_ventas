cc644e4a43933cee421e3dff348787b29e43a690
yormanau <109599510+yormanau@users.noreply.github.com>
2025-06-30 11:16:27 -0500

    Moviendo a raíz

---
+++ utils/mysql/index.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2')
+require('dotenv').config();
+const conexion = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT
+})
+
+conexion.connect((err) => {
+    if (err) {
+        console.error('Error de conexión: ', err)
+        return;
+    }
+    console.log('Conexión exitosa')
+})
+
+module.exports = { conexion };
