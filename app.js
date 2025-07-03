3ac344cae1d6cc3aa66cb271b2046c4dad692b3f
yormanau <109599510+yormanau@users.noreply.github.com>
2025-07-02 20:09:35 -0500

    configurando mysqladapter

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const { createBot, createProvider, createFlow } = require('@bot-whatsapp/bot')
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const adapterProvider = createProvider(BaileysProvider)
-const MockAdapter = require('@bot-whatsapp/database/json')
+const MockAdapter = require('@bot-whatsapp/database/mysql')
 const { escuchar_mensajes } = require('./utils/escuchar_mensajes.js')
 const { syncData } = require('./utils/sheets.js')
 module.exports = adapterProvider;
 syncData()
+
+
 
 // Importar flujos
 const flujo_inicio = require('./Flujos/01_inicio.js')
@@ -18,7 +20,16 @@ const flujo_activar = require('./Flujos/humano.js')
 
 
 const main = async () => {
-    const adapterDB = new MockAdapter()
+    const adapterDB = new MySQLAdapter({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        port: process.env.PORT,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    })
     const adapterFlow = createFlow([
         flujo_inicio,
         flujo_bienvenida,
@@ -40,6 +51,3 @@ const main = async () => {
 }
 
 main()
-/* 
-
-*/
